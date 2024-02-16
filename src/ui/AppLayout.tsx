993b9a7c8337c3ea2c048ba9a71a5579db6dfba4
divyuk <div14kes@gmail.com>
2024-02-16 15:45:50 +0530

    App Layout for the App, header and sidebar and outlet 📊

--- src/ui/AppLayout.tsx
+++ src/ui/AppLayout.tsx
@@ -1,10 +1,29 @@
 import { Outlet } from "react-router-dom";
+import Header from "./Header";
+import SideBar from "./SideBar";
+import styled from "styled-components";
 
-function AppLayout() {
+const Main = styled.main`
+  background-color: green;
+  padding: 4rem 4.8rem 6.4rem;
+`;
+
+const StyledAppLayout = styled.div`
+  display: grid;
+  grid-template-columns: 26rem 1fr;
+  grid-template-rows: auto 1fr;
+  height: 100vh;
+`;
+
+function AppLayout(): JSX.Element {
   return (
-    <div>
-      <Outlet />
-    </div>
+    <StyledAppLayout>
+      <Header />
+      <SideBar />
+      <Main>
+        <Outlet />
+      </Main>
+    </StyledAppLayout>
   );
 }
 
