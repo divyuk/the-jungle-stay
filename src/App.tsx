8bbb3da500be8bc8fb0a108608e7ec7cec6e1496
divyuk <div14kes@gmail.com>
2024-02-16 15:00:03 +0530

    Created the Landing Environment | Routes and paths 🎰

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,36 @@
-import styled from "styled-components";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+
 import GlobalStyles from "./styles/GlobalStyles";
-import Button from "./ui/Button.tsx";
-import Heading from "./ui/Heading.tsx";
+import Dashboard from "./pages/Dashboard";
+import Bookings from "./pages/Bookings";
+import Cabins from "./pages/Cabins";
+import Users from "./pages/Users";
+import Settings from "./pages/Settings";
+import Account from "./pages/Account";
+import Login from "./pages/Login";
+import PageNotFound from "./pages/PageNotFound";
+import AppLayout from "./ui/AppLayout";
 
-const StyledApp = styled.main`
-  background-color: #ddd3cf;
-  padding: 20px;
-`;
-
-function App() {
+function App(): JSX.Element {
   return (
     <>
       <GlobalStyles />
-      <StyledApp>
-        <Heading as="h1">The Jungle Stay</Heading>
-        <Heading as="h2">Check in</Heading>
-        <Heading as="h3">Check out</Heading>
-        <Button>Check In</Button>
-      </StyledApp>
+      <BrowserRouter>
+        <Routes>
+          <Route element={<AppLayout />}>
+            <Route index element={<Navigate replace to="dashboard" />} />
+            <Route path="dashboard" element={<Dashboard />} />
+            <Route path="bookings" element={<Bookings />} />
+            <Route path="cabins" element={<Cabins />} />
+            <Route path="users" element={<Users />} />
+            <Route path="settings" element={<Settings />} />
+            <Route path="account" element={<Account />} />
+          </Route>
+
+          <Route path="login" element={<Login />} />
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
