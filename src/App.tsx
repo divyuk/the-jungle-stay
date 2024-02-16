c9b5b167806aad54e07ba31158f8b4894d8af24b
divyuk <div14kes@gmail.com>
2024-02-16 14:18:17 +0530

    Testing 🧪

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import styled from "styled-components";
 import GlobalStyles from "./styles/GlobalStyles";
+import Button from "./ui/Button.tsx";
+import Heading from "./ui/Heading.tsx";
 
 const StyledApp = styled.main`
-  background-color: orangered;
+  background-color: #ddd3cf;
   padding: 20px;
 `;
 
@@ -10,7 +12,12 @@ function App() {
   return (
     <>
       <GlobalStyles />
-      <StyledApp>Hello World</StyledApp>
+      <StyledApp>
+        <Heading as="h1">The Jungle Stay</Heading>
+        <Heading as="h2">Check in</Heading>
+        <Heading as="h3">Check out</Heading>
+        <Button>Check In</Button>
+      </StyledApp>
     </>
   );
 }
