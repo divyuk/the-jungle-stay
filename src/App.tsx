477220604486f150fc5a5f0f91af48114b70b44a
divyuk <div14kes@gmail.com>
2024-02-16 13:06:26 +0530

    Main app file applying Global CSS 🎋

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,18 @@
+import styled from "styled-components";
+import GlobalStyles from "./styles/GlobalStyles";
+
+const StyledApp = styled.main`
+  background-color: orangered;
+  padding: 20px;
+`;
+
 function App() {
-  return <div>Hello World</div>;
+  return (
+    <>
+      <GlobalStyles />
+      <StyledApp>Hello World</StyledApp>
+    </>
+  );
 }
 
 export default App;
