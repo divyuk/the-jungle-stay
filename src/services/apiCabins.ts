15db5758f47df51b34045f1fe84bd8a26c3384f1
divyuk <div14kes@gmail.com>
2024-02-17 13:37:01 +0530

    Logic for deleting Cabins ❌

--- src/services/apiCabins.ts
+++ src/services/apiCabins.ts
@@ -8,3 +8,12 @@ export async function getCabins() {
   }
   return data;
 }
+
+export async function deleteCabin(id) {
+  const { data, error } = await supabase.from("cabins").delete().eq("id", id);
+  if (error) {
+    console.log(error);
+    throw new Error("Cabins could not be loaded!");
+  }
+  return data;
+}
