c951453f31db105fb6bc2b8ec14cb1cd813cdc01
divyuk <div14kes@gmail.com>
2024-02-17 21:47:54 +0530

    Cabins form refactoring 🔰✨✨

--- src/services/apiCabins.ts
+++ src/services/apiCabins.ts
@@ -9,6 +9,13 @@ export async function getCabins() {
   return data;
 }
 
+export async function createCabin(newCabin) {
+  const { data, error } = await supabase
+    .from("cabins")
+    .insert([newCabin])
+    .select();
+}
+
 export async function deleteCabin(id) {
   const { data, error } = await supabase.from("cabins").delete().eq("id", id);
   if (error) {
