e98fcee768976e0675fbf4c6e42b99e130a21514
divyuk <div14kes@gmail.com>
2024-02-19 17:19:43 +0530

    exporting superbase url 🔰

--- src/services/superbase.ts
+++ src/services/superbase.ts
@@ -1,5 +1,5 @@
 import { createClient } from "@supabase/supabase-js";
-const supabaseUrl = "https://uyeivyzcmejtqtjpmtys.supabase.co";
+export const supabaseUrl = "https://uyeivyzcmejtqtjpmtys.supabase.co";
 const supabaseKey: string = import.meta.env.VITE_SUPABASE_KEY;
 const supabase = createClient(supabaseUrl, supabaseKey);
 
