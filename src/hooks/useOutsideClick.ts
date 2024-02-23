08a2621b9a7a7049f3f81121954355616fdd2798
divyuk <div14kes@gmail.com>
2024-02-23 22:06:50 +0530

    Reusable hook for the outside click ⛎

---
+++ src/hooks/useOutsideClick.ts
@@ -0,0 +1,22 @@
+import { useEffect, useRef } from "react";
+
+export function useOutsideClick(handler, listenCapturing = true) {
+  const ref = useRef();
+
+  useEffect(
+    function () {
+      function handleClick(e) {
+        if (ref.current && !ref.current.contains(e.target)) {
+          console.log("Click Outside");
+          handler();
+        }
+      }
+      document.addEventListener("click", handleClick, listenCapturing); // true so that the event
+      // gets captured during capturing phase and not in bubbling phase
+      return () =>
+        document.removeEventListener("click", handleClick, listenCapturing);
+    },
+    [handler]
+  );
+  return ref;
+}
