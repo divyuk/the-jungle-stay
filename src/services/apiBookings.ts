5eb231afeda8a17f431b68ccefff0aec84fb20e0
divyuk <div14kes@gmail.com>
2024-02-24 09:53:37 +0530

    🎪 api side filtering ! apiBookings.ts

--- src/services/apiBookings.ts
+++ src/services/apiBookings.ts
@@ -1,11 +1,17 @@
 import { getToday } from "../utils/helpers";
 import supabase from "./supabase";
-export async function getBookings() {
-  const { data, error } = await supabase
+export async function getBookings({ filter, sorrtBy }) {
+  let query = supabase
     .from("bookings")
     .select(
       "id, created_at, startDate, endDate, numNights, status, totalPrice, cabins(name) , guests(fullName, email)"
     );
+
+  // Filter
+  if (filter !== null)
+    query = query[filter.method || "eq"](filter.field, filter.value);
+  const { data, error } = await query;
+
   if (error) {
     console.error(error);
     throw new Error("Bookings could not be loaded at this time");
