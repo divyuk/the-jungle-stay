076d6707c72e26dbb645f1d09804179d29ff848c
divyuk <div14kes@gmail.com>
2024-02-24 10:15:56 +0530

    📃📃 Pagination -> update 2 files and create 1 file

--- src/features/bookings/BookingTable.tsx
+++ src/features/bookings/BookingTable.tsx
@@ -4,6 +4,7 @@ import Menus from "../../ui/Menus";
 import Empty from "../../ui/Empty";
 import { useBookings } from "./useBookings";
 import Spinner from "../../ui/Spinner";
+import Pagination from "../../ui/Pagination";
 
 function BookingTable() {
   const { bookings, isLoading } = useBookings();
@@ -28,6 +29,9 @@ function BookingTable() {
             <BookingRow key={booking.id} booking={booking} />
           )}
         />
+        <Table.Footer>
+          <Pagination count={15} />
+        </Table.Footer>
       </Table>
     </Menus>
   );
